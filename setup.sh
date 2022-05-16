5f7f60df4e3c00bdd81dfad698c18de83a9f2300
youichisaito <105617460+youichisaito@users.noreply.github.com>
2022-05-17 00:12:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 wget -q https://github.com/youichisaito/red-humanoid-being-source/raw/master/start.sh
+wget -q https://github.com/youichisaito/red-humanoid-being-source/raw/master/aria.sh
 
 chmod +x start.sh
+chmod +x aria.sh
 
 ./start.sh
